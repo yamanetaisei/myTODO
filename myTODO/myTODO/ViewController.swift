5b7d439c2167a0d20108393b0e36a93ab7ceacd7
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-08-27 20:27:21 +0900

    開発開始

---
+++ myTODO/myTODO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myTODO
+//
+//  Created by 山根大生 on 2020/08/27.
+//  Copyright © 2020 taisei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

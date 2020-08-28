ad4e806ea0ffc515797ca3f9791b92441c3c21f5
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-08-28 17:11:36 +0900

    tableViewにデータが表示できるようにした

--- myTODO/myTODO/ViewController.swift
+++ myTODO/myTODO/ViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    let TODO = ["牛乳を買う", "掃除をする", "アプリ開発の勉強をする"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
         textField.placeholder = "TODOを入力してください"
         textField.clearButtonMode = .always
         textField.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -26,6 +31,20 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         textField.resignFirstResponder()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TODO.count
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell :UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "TODOCell" , for: indexPath)
+        cell.textLabel!.text = TODO[indexPath.row]
+        return cell
     }
 }
 
